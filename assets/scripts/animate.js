70b212a0de4a6aaa6a9c6ad6b56bfc3e0c301c1e
Savannah Scott <sascott624@gmail.com>
2018-01-20 12:28:27 -0500

    Create typewriter function in animate.js

--- assets/scripts/animate.js
+++ assets/scripts/animate.js
@@ -3,8 +3,24 @@ class Animate {
     this.letters = letters
 
     window.setTimeout(() => {
-      this.finalPlacement()
+      this.typewriter()
     }, 1000)
+  }
+
+  typewriter() {
+    for(let i = 0; i < this.letters.length; i++) {
+      const letter = this.letters[i]
+      const previous = grid[i - 1] ? grid[i - 1] : { letter: '' }
+      const timeout = previous.letter === grid[i].letter ? 0.15 : previous.letter === 'h' ? 0.80 : Math.random()
+
+      window.setTimeout(() => {
+        letter.style.width = grid[i].width
+        letter.style.height = grid[i].height
+
+        letter.style.left = grid[i][1].left
+        letter.style.top = grid[i][1].top
+      }, 500 * (i + timeout))
+    }
   }
 
   finalPlacement() {
